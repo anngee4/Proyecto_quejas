2cd69962766358fe8273631e50d2ba40c783e86b
anngee4 <angelin.ramirez2309@gmail.com>
2025-06-13 23:56:51 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
   const firebaseConfig = {
-    apiKey: "AIzaSyBZEVISZuRfn1iC3be5b7xTMQiCZubXyTU",
-    authDomain: "bdnosql-de619.firebaseapp.com",
-    projectId: "bdnosql-de619",
-    storageBucket: "bdnosql-de619.firebasestorage.app",
-    messagingSenderId: "519369652548",
-    appId: "1:519369652548:web:260c2b45275f5a0ff5ff11",
-    measurementId: "G-1FYEJLG220"
+    apiKey: "AIzaSyC6cIoc_xfZQq4Nqm8mGQ8YnNO_UjE93uM",
+    authDomain: "my-bd1-2150c.firebaseapp.com",
+    projectId: "my-bd1-2150c",
+    storageBucket: "my-bd1-2150c.firebasestorage.app",
+    messagingSenderId: "755243371902",
+    appId: "1:755243371902:web:cdd72c4d1cae265fd1b72f",
+    measurementId: "G-T09N32TS17"
   };
 
 const app = initializeApp(firebaseConfig);
